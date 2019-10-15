febbe68fb62f3f7f4c112ed2025df3dd45414271
Peter Cunha <petercunha8@gmail.com>
2019-10-14 22:06:07 -0700

    Update logging

--- lambda/index.js
+++ lambda/index.js
@@ -1,5 +1,5 @@
 // Import request module
-const request = require('request');
+const request = require('request')
 
 /**
  * Responds to any HTTP request.
@@ -9,50 +9,52 @@ const request = require('request');
  */
 
 exports.tts = (req, res) => {
-	// Enable CORS
-	res.set('Access-Control-Allow-Origin', 'https://www.textreader.pro');
-	res.set('Access-Control-Allow-Origin', 'https://textreader.pro');
-	res.set('Access-Control-Allow-Methods', 'GET, POST');
-	res.set(
-		'Access-Control-Allow-Headers',
-		'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
-	);
+    // Enable CORS
+    res.set('Access-Control-Allow-Origin', 'https://www.textreader.pro')
+    res.set('Access-Control-Allow-Origin', 'https://textreader.pro')
+    res.set('Access-Control-Allow-Methods', 'GET, POST')
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
 
-	// Intercept non-POST requests
-	if (req.method != 'POST') {
-		res.send('ok');
-		return;
-	}
+    // Intercept non-POST requests
+    if (req.method != 'POST') {
+        res.send('ok')
+        return
+    }
 
-	// Prepare request params for Streamlabs
-	let voice = req.body.voice || 'Brian';
-	let text = req.body.text || 'Please select a voice and message.';
+    // Prepare request params for Streamlabs
+    let voice = req.body.voice || 'Brian'
+    let text = req.body.text || 'Please select a voice and message.'
 
-	// Send request to Streamlabs' API server
-	request.post(
-		'https://streamlabs.com/polly/speak',
-		{ json: { voice: voice, text: text } },
-		function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				res.status(200).json(body);
-			} else {
-				res.status(500).json({
-					success: false,
-					reason: 'Streamlabs API error.',
-					error: error,
-					code: response.statusCode
-				});
-			}
-		}
-	);
+    // Send request to Streamlabs' API server
+    request.post(
+        'https://streamlabs.com/polly/speak', {
+            json: {
+                voice: voice,
+                text: text
+            }
+        },
+        function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.status(200).json(body);
+            } else {
+                res.status(500).json({
+                    success: false,
+                    reason: 'Streamlabs API error.',
+                    error: error,
+                    code: response.statusCode
+                })
+            }
+        }
+    )
 
-	// Log output to console
-	const jsonLog = {
-		payload: {
-			voice: voice,
-			text: text
-		},
-		requestIP: req.ip
-	};
-	console.log('Request Log:\n' + JSON.stringify(jsonLog));
-};
+    // Log output to console
+    const jsonLog = {
+        payload: {
+            voice: voice,
+            text: text
+        },
+        requestIP: req.ip
+    }
+    console.info(`${voice}: ${text}`)
+    console.debug(jsonLog)
+}
